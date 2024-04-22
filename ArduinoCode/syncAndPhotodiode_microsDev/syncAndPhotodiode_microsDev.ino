d660f832c2ec60059fc9df10e75e0c344e372b66
tomasaso <ucbteho@ucl.ac.uk>
2024-04-22 16:59:57 +0100

    Create syncAndPhotodiode_microsDev.ino

---
+++ ArduinoCode/syncAndPhotodiode_microsDev/syncAndPhotodiode_microsDev.ino
@@ -0,0 +1,49 @@
+//#include <Event.h>
+//#include <Timer.h>
+
+#define photodiodePin 0        // PhotoDiode pin - analogue
+#define syncPin 3             // digital pin of sync
+
+// Serial communication variables
+float samplingFrequency = 2000; // frequency to send new values to computer
+const long interval = 1000000 / (samplingFrequency);  // sampling interval to send new values
+unsigned long startMillis;  // sample timer that resets each time new data is sent
+unsigned long currentMillis; // rolling timer to check if it's time to send new data
+
+/*
+int photodiodePin = 0;    
+int syncPin = 0;
+*/
+
+int photodiodeVal;
+int syncVal;
+
+void setup()
+
+{
+  pinMode(photodiodePin, INPUT);   // PhotoDiode
+  pinMode(syncPin, INPUT);   // digital pin of sync
+  
+  Serial.begin(250000);          //  setup serial
+  delay(500);
+}
+
+
+
+void loop()
+
+{
+
+  photodiodeVal = analogRead(photodiodePin);    // read the input pin
+  syncVal = digitalRead(syncPin);
+  currentMillis = micros();                   //get the current "time" (actually the number of milliseconds since the program started)
+  if (currentMillis - startMillis >= interval)  //test whether the period has elapsed
+  {
+  Serial.print(photodiodeVal);
+  Serial.print(",");
+  Serial.println(syncVal);
+  //delay(0);
+      startMillis = currentMillis;  // reset timer
+  }
+  
+}
