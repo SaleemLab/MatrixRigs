5d7f8e23d74551b93248ada31f7ebb013b75be16
ssriranga <77621614+SSsriranga@users.noreply.github.com>
2025-04-11 17:56:01 +0100

    Added TwoPFrameTime to EyeTracking Arduino

--- ArduinoCode/2pRig/EyeTrackingArduino_SyncReceiver_April2025/EyeTrackingArduino_SyncReceiver_April2025.ino
+++ ArduinoCode/2pRig/EyeTrackingArduino_SyncReceiver_April2025/EyeTrackingArduino_SyncReceiver_April2025.ino
@@ -1,4 +1,5 @@
 #define SyncPin 6
+#define FrameTrig 9   
 
 unsigned long currentMillis;
 unsigned long lastSyncPulseTime;
@@ -6,11 +7,18 @@ unsigned long startMillis;
 float samplingFrequency = 120;
 const long interval = 1000 / (samplingFrequency);
 
+// variables for frame trigger and line trigger counters 
+unsigned long last2pFrameTime;   // updates each time 2p frame goes LOW
+
 void setup() {
   // put your setup code here, to run once:
 pinMode(SyncPin, INPUT);
+pinMode(FrameTrig, INPUT);              // frame trigger 
 
+// interrupts async pulse 
 attachInterrupt(digitalPinToInterrupt(SyncPin), SyncPulse_Receiver, CHANGE);
+// interrupt for frame and line scanner trigger 
+attachInterrupt(digitalPinToInterrupt(FrameTrig), Frame_Counter, FALLING);
 
 Serial.begin(1000000);
 startMillis = millis();
@@ -23,6 +31,8 @@ if (currentMillis - startMillis >= interval)
 {
   Serial.print(lastSyncPulseTime);
   Serial.print("\t");
+  Serial.print(last2pFrameTime);
+  Serial.print("\t");
   Serial.print(currentMillis);
   //Serial.print(",");
   Serial.println("\n");
@@ -33,4 +43,12 @@ if (currentMillis - startMillis >= interval)
 
 void SyncPulse_Receiver(){
   lastSyncPulseTime = millis();
+}
+
+/////////////////////////////////////////
+/// 2p frame time recording functions ///
+/////////////////////////////////////////
+
+void Frame_Counter() { // Interrupt for when frame trigger goes down and line trigger goes LOW
+  last2pFrameTime = millis();
 }
