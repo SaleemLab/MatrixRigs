d9ff7fbc585f5a1c697f1e5abe45409c15622452
ssriranga <77621614+SSsriranga@users.noreply.github.com>
2025-04-20 20:58:07 +0100

    removed TwoPFrameTime from EyeCamSync

--- ArduinoCode/2pRig/EyeTrackingArduino_SyncReceiver_April2025/EyeTrackingArduino_SyncReceiver_April2025.ino
+++ ArduinoCode/2pRig/EyeTrackingArduino_SyncReceiver_April2025/EyeTrackingArduino_SyncReceiver_April2025.ino
@@ -1,5 +1,5 @@
 #define SyncPin 6
-#define FrameTrig 9   
+//#define FrameTrig 9   
 
 unsigned long currentMillis;
 unsigned long lastSyncPulseTime;
@@ -8,17 +8,17 @@ float samplingFrequency = 120;
 const long interval = 1000 / (samplingFrequency);
 
 // variables for frame trigger and line trigger counters 
-unsigned long last2pFrameTime;   // updates each time 2p frame goes LOW
+//unsigned long last2pFrameTime;   // updates each time 2p frame goes LOW
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(SyncPin, INPUT);
-pinMode(FrameTrig, INPUT);              // frame trigger 
+//pinMode(FrameTrig, INPUT);              // frame trigger 
 
 // interrupts async pulse 
 attachInterrupt(digitalPinToInterrupt(SyncPin), SyncPulse_Receiver, CHANGE);
 // interrupt for frame and line scanner trigger 
-attachInterrupt(digitalPinToInterrupt(FrameTrig), Frame_Counter, FALLING);
+//attachInterrupt(digitalPinToInterrupt(FrameTrig), Frame_Counter, FALLING);
 
 Serial.begin(1000000);
 startMillis = millis();
@@ -31,8 +31,8 @@ if (currentMillis - startMillis >= interval)
 {
   Serial.print(lastSyncPulseTime);
   Serial.print("\t");
-  Serial.print(last2pFrameTime);
-  Serial.print("\t");
+  //Serial.print(last2pFrameTime);
+  //Serial.print("\t");
   Serial.print(currentMillis);
   //Serial.print(",");
   Serial.println("\n");
@@ -49,6 +49,6 @@ void SyncPulse_Receiver(){
 /// 2p frame time recording functions ///
 /////////////////////////////////////////
 
-void Frame_Counter() { // Interrupt for when frame trigger goes down and line trigger goes LOW
-  last2pFrameTime = millis();
-}
+//void Frame_Counter() { // Interrupt for when frame trigger goes down and line trigger goes LOW
+//  last2pFrameTime = millis();
+//
